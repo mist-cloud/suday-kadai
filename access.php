3451e0f6c5b2c9577d9ee9bf26b211f7c7cd4436
吉田潤二 <>
2018-04-18 16:57:13 +0900

    4-2 データベースに接続する

---
+++ access.php
@@ -0,0 +1,24 @@
+<?php
+$USER       =   'root';
+$PW         =   '';
+$dnsinfo    =   "mysql:dbname=salesmanagement;host=localhost;charset=utf8";
+try{
+    $pd     =   new PDO($dnsinfo,$USER,$PW);
+    $res    =   "接続しました";
+}catch(PDOException $e){
+    $res    =   $e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>始めようPHP</title>
+</head>
+<body>
+    <h1>PHPでMySQLに接続する</h1>
+    <?php
+    echo $res;
+    ?>
+</body>
+</html>
